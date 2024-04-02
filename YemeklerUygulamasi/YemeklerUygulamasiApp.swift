9ca9c0085154a80d48f6103701dd85dd5b1b64bc
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2024-04-02 23:27:29 +0300

    Initial Commit

---
+++ YemeklerUygulamasi/YemeklerUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YemeklerUygulamasiApp.swift
+//  YemeklerUygulamasi
+//
+//  Created by Tuğçe Saygın on 2.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct YemeklerUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
